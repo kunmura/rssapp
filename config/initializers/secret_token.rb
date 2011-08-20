fe9564c8483421d791aae0faba8bbbed82a1b02c
kunmura <tvfrog99@gmail.com>
2011-08-20 13:25:24 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rssapp::Application.config.secret_token = 'ea7fbd2e70929d79fede7b3c051eae5a803b54fc0ceb92159f30e0b7e746b8fe1c1c2155aca770fcbaa2184075065212b08f0eedb782b35eccebde5ced0a29ce'
